5ce1f776cb7aeb14f8f1018ae5499eb1c9dee90b
Jose Maria De Lùcas Plata <d3lucasx@gmail.com>
2025-09-21 14:31:40 +0200

    Sigo con los ejercicios de Acceso a datos de lectura de ficheros

--- eclipse-workspace/AccesoADatos/src/EjerciciosDeLecturaYEscritura_SegundaTarea/ejercicio2_numeroCaracteresYvocales.java
+++ eclipse-workspace/AccesoADatos/src/EjerciciosDeLecturaYEscritura_SegundaTarea/ejercicio2_numeroCaracteresYvocales.java
@@ -13,25 +13,24 @@ public class ejercicio2_numeroCaracteresYvocales {
 	public static void main(String[] args) {
 		String regex = "[aeiouAEIOU]";
 		String LineaSinEspacios;
-		int cantidadVocales = 0;
-		File ficheroAleer = new File("C:/Users/DAM/Downloads/pepe.txt");
+		int cantidadVocales = 0; //C:/Users/DAM/Downloads/pepe.txt
+		File ficheroAleer = new File("C:/Users/d3luc/Downloads/PEPE.txt");
 		try {
 			FileReader lector =new FileReader(ficheroAleer);
 			BufferedReader buffer = new BufferedReader(lector);
 			String linea;
+			Pattern pattern = Pattern.compile(regex);
 			
 			while((linea = buffer.readLine()) != null ) {
 				LineaSinEspacios = linea.replaceAll(" ", "");
 				System.out.println(LineaSinEspacios);
-				linea = buffer.readLine();
+				Matcher matcher = pattern.matcher(LineaSinEspacios);
+				while (matcher.find()) {
+					cantidadVocales++;
+				}
 			}
+			System.out.println("Cantidad de vocales = " + cantidadVocales);
 			
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(linea);
-			while (matcher.find()) {
-				cantidadVocales++;
-				
-			}
 		}catch(IOException e) {
 			e.getMessage();
 		}
---
+++ eclipse-workspace/AccesoADatos/src/EjerciciosDeLecturaYEscritura_SegundaTarea/ejercicio3_campoValor.java
@@ -0,0 +1,37 @@
+package EjerciciosDeLecturaYEscritura_SegundaTarea;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ejercicio3_campoValor {
+
+	public static void main(String[] args) {
+		File ficheroAleer = new File("C:/Users/d3luc/Downloads/Restaurants.csv");
+		try {
+			FileReader lector =new FileReader(ficheroAleer);
+			BufferedReader buffer = new BufferedReader(lector);
+			String cabecera = buffer.readLine(); // capturamos la primera linea donde se encuentra los titulos de los campos
+			String[] campos = cabecera.split(","); // Separamos la cabecera en campos.
+			
+			String linea;
+			while((linea = buffer.readLine()) != null ) {
+				
+				// Con esta expresion regular, evitamos que divida por comas que hay dentro de un campo, por ejemplo "Alcala de Henares, Madrid"
+				// pues que el campo direccion sea alcala de henares, madrid y no
+				// campo direccion : Alcala de henares
+				// otro campo: madrid
+				String[] regex = linea.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+				for (int i = 0; i < campos.length && i < regex.length; i++) {
+					System.out.println(campos[i] + ": " + regex[i].replace("\"", "").trim());
+				}
+				System.out.println("------------------------"); // separador entre registros
+			}
+		}catch(IOException e) {
+			e.getMessage();
+		}
+
+	}
+
+}
