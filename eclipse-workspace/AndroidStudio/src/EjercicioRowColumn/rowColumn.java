4b0c5e52ac3cc8aceb8a5f0bd3c2a444726696a4
D3LucasX <d3Lucasx@gmail.com>
2025-09-22 14:29:21 +0200

    Clase de Android studio

--- eclipse-workspace/AndroidStudio/src/EjercicioRowColumn/rowColumn.java
+++ eclipse-workspace/AndroidStudio/src/EjercicioRowColumn/rowColumn.java
@@ -1,10 +1,13 @@
 package com.example.ejemplo3
 
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,20 +15,30 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.TopStart
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ejemplo3.ui.theme.Ejemplo3Theme
@@ -72,19 +85,41 @@ fun EjemploBox(){
 
 @Composable
 fun imagenConTexto(){
-
-    Box(modifier = Modifier.fillMaxSize().padding( 40.dp)){
+    var backgroundBoxClor by remember { mutableStateOf(Color.White) }
+    var textPosicion by remember { mutableStateOf (Offset(0f, 0f) ) }
+    Box(modifier = Modifier.fillMaxSize().padding( 40.dp).background(backgroundBoxClor)){
         Image(
             painter= painterResource(id = R.drawable.imagen_tortuga),
             contentDescription ="tortu",
             modifier = Modifier.align(Center).fillMaxSize()
         )
+
         Text(text = "tortuga andando",
             fontSize = 22.sp,
             color = Color.Green,
-            modifier = Modifier.align(Alignment.Center)
+            textAlign = TextAlign.Center,
+            modifier = Modifier.offset{
+                IntOffset(textPosicion.x.toInt(), textPosicion.y.toInt())
+            }.pointerInput(Unit){
+                detectDragGestures { change, dragAmount ->  }
+            }
             )
+        Button(
+            onClick = { backgroundBoxClor = randomColor()},
+            modifier = Modifier.align(Alignment.TopStart)
+        )
+        {
+            Text(text= "Cambiar Fondo")
+        }
     }
+}
+
+fun randomColor(): Color {
+    val rojo = (0..255).random()
+    val azul = (0..255).random()
+    val verde = (0..255).random()
+
+    return Color(red= rojo, blue= azul, green= verde)
 }
 
 @Preview
