6aedb9cb9780918c4a13a581a8d62b0390a4dff4
Jose Maria De Lùcas Plata <d3lucasx@gmail.com>
2025-10-09 20:09:03 +0200

    Hago merge

--- eclipse-workspace/PSP8_EjercicioHilosEncuesta/src/EncuestaConcurrencia/Hilo.java
+++ eclipse-workspace/PSP8_EjercicioHilosEncuesta/src/EncuestaConcurrencia/Hilo.java
@@ -1,41 +1,167 @@
 package EncuestaConcurrencia;
 
-public class Hilo implements Runnable{
-	int numVotantes;
-	int zona;
-	int partido;
-	int voto;
-	int votosXzona = 0;
+public class Hilo implements Runnable {
+	int nombreZona;
+	int num_votantes;
 	ResultadosEncuesta resultados;
-	
 
-	public Hilo(String zona, int numVotantes, int voto, int partido) {
+	public Hilo(String nombreZona,int num_votantes, ResultadosEncuesta resultados) {
+		
 		super();
-		this.zona = ((int) Math.random()*4);
-		this.numVotantes = numVotantes;
-		this.voto = ((int) Math.random()*2);
-		this.partido = ((int) Math.random()*4);
+		this.nombreZona = (int) (Math.random() * 4);
+		this.num_votantes = num_votantes;
+		this.resultados = resultados;
 	}
-
 
 	@Override
 	public void run() {
-		String nombreZona = "";
+		int votos
 		if (zona == 0) {
 			nombreZona = "Norte";
-		}else if (zona == 1) {
+			num_votantes = 300;
+		} else if (zona == 1) {
 			nombreZona = "Sur";
-		}else if (zona == 2) {
+			num_votantes = 400;
+		} else if (zona == 2) {
 			nombreZona = "Este";
-		}else if (zona == 3) {
+			num_votantes = 500;
+		} else if (zona == 3) {
 			nombreZona = "Oeste";
+			num_votantes = 350;
 		}
-		
-		if (voto == 1) {
+
+		if (esVoto == 1) {
 			this.resultados.incementarVotos();
+			votoPositivo++;
 			this.votosXzona++;
+
+			if (zona == 0) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPposi++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoeposi++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxposi++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarposi++;
+				}
+			} else if (zona == 1) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPposi++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoeposi++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxposi++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarposi++;
+				}
+			} else if (zona == 2) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPposi++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoeposi++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxposi++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarposi++;
+				}
+			} else if (zona == 3) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPposi++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoeposi++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxposi++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarposi++;
+				}
+			}
+
+		} else if (esVoto == 2) {
+			this.resultados.incementarVotos();
+			votosXzona++;
+			votoNulo++;
+			if (zona == 0) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPnega++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoenega++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxnega++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarnega++;
+				}
+			}else if (zona == 1) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPnega++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoenega++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxnega++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarnega++;
+				}
+			}else if (zona == 2) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPnega++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoenega++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxnega++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarnega++;
+				}
+			}else if (zona == 3) {
+				if (partido == 0) {
+					nombrePartido="PP";
+					votoPPnega++;
+				} else if (partido == 1) {
+					nombrePartido = "PESOE";
+					votoPesoenega++;
+				} else if (partido == 2) {
+					nombrePartido = "VOX";
+					votoVoxnega++;
+				} else if (partido == 3) {
+					nombrePartido = "Sumar";
+					votoSumarnega++;
+				}
+			}
+		}else {
+			votoNulo++;
 		}
-		System.out.println("Votos recolectados en esta zona.");
+		System.out.printf("Resumen de votacion en la zona %s:\n", nombreZona
+				+ "\t %s; ", nombrePartido
+				+ "\t\t Votos a favor: %d."
+				+ "\t\t Votos en contra: %d."
+				+ "");
 	}
 
 }
--- eclipse-workspace/PSP8_EjercicioHilosEncuesta/src/EncuestaConcurrencia/ResultadosEncuesta.java
+++ eclipse-workspace/PSP8_EjercicioHilosEncuesta/src/EncuestaConcurrencia/ResultadosEncuesta.java
@@ -1,14 +1,24 @@
 package EncuestaConcurrencia;
 
 public class ResultadosEncuesta {
-private int resultado = 0;
+	private int resultado = 0;
+	private int votosNegativos;
+	private int votosPositivos;
+	private int votossNulos;
 
+	synchronized public void incementarVotos() {
+		resultado++;
+	}
+
+	public synchronized void incrementarPositivos() {
+		votosPositivos++;
+	}
+
+	public synchronized void incrementarNegativos() {
+		votosNegativos++;
+	}
 
 	synchronized public int getcuenta() {
-	return this.resultado;
-	}
-	synchronized public int incementarVotos() {
-		resultado++;
-		return resultado;
+		return this.resultado;
 	}
 }
